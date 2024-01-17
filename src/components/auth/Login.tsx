9619d66c3619c73f0228039ce9f86b2ec4af9107
Janne <janne.lahdentausta25@gmail.com>
2024-01-17 14:26:44 +0100

    Add keyboard dismiss to login page

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
-import { TextInput, View, StyleSheet, Text } from "react-native";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Text,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import FormStyles from "../../styles/Forms";
 import Button from "../Button";
 import TextStyles from "../../styles/Text";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import { login } from "../../services/auth";
 import useAuth from "../../hooks/useAuth";
 
 const LoginSchema = Yup.object().shape({
@@ -17,64 +22,66 @@ const Login = () => {
   const { fetchingLogin, errorLogin, login } = useAuth();
 
   return (
-    <View style={styles.container}>
-      <Text style={TextStyles.h1}>Welcome to Resty</Text>
-      <Text style={{ ...TextStyles.body, paddingBottom: 30, paddingTop: 15 }}>
-        Please authenticate to access the platform
-      </Text>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Text style={TextStyles.h1}>Welcome to Resty</Text>
+        <Text style={{ ...TextStyles.body, paddingBottom: 30, paddingTop: 15 }}>
+          Please authenticate to access the platform
+        </Text>
 
-      <View style={styles.formComponent}>
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          validationSchema={LoginSchema}
-          onSubmit={(values) => login(values.email, values.password)}
-        >
-          {({ handleChange, handleBlur, handleSubmit, errors, values }) => (
-            <>
-              <TextInput
-                style={FormStyles.input}
-                placeholder="Email"
-                value={values.email}
-                testID="email"
-                onChangeText={handleChange("email")}
-                onBlur={handleBlur("email")}
-              />
-              {errors.email && (
-                <Text style={FormStyles.error}>{errors.email}</Text>
-              )}
+        <View style={styles.formComponent}>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={LoginSchema}
+            onSubmit={(values) => login(values.email, values.password)}
+          >
+            {({ handleChange, handleBlur, handleSubmit, errors, values }) => (
+              <>
+                <TextInput
+                  style={FormStyles.input}
+                  placeholder="Email"
+                  value={values.email}
+                  testID="email"
+                  onChangeText={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                />
+                {errors.email && (
+                  <Text style={FormStyles.error}>{errors.email}</Text>
+                )}
 
-              <TextInput
-                style={FormStyles.input}
-                value={values.password}
-                testID="password"
-                placeholder="Password"
-                secureTextEntry
-                onChangeText={handleChange("password")}
-                onBlur={handleBlur("password")}
-              />
-              {errors.password && (
-                <Text style={{ ...FormStyles.error, paddingBottom: 12 }}>
-                  {errors.password}
-                </Text>
-              )}
+                <TextInput
+                  style={FormStyles.input}
+                  value={values.password}
+                  testID="password"
+                  placeholder="Password"
+                  secureTextEntry
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                />
+                {errors.password && (
+                  <Text style={{ ...FormStyles.error, paddingBottom: 12 }}>
+                    {errors.password}
+                  </Text>
+                )}
 
-              {errorLogin && (
-                <Text style={{ ...FormStyles.error, paddingBottom: 12 }}>
-                  Invalid email or password
-                </Text>
-              )}
+                {errorLogin && (
+                  <Text style={{ ...FormStyles.error, paddingBottom: 12 }}>
+                    Invalid email or password
+                  </Text>
+                )}
 
-              <Button
-                testID="submit"
-                text="Login"
-                onPress={handleSubmit}
-                loading={fetchingLogin}
-              />
-            </>
-          )}
-        </Formik>
+                <Button
+                  testID="submit"
+                  text="Login"
+                  onPress={handleSubmit}
+                  loading={fetchingLogin}
+                />
+              </>
+            )}
+          </Formik>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
